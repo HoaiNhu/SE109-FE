383372676d8337759cf735b96d8b58054bdbf657
tamnhu11204 <22521053@gm.uit.edu.vn>
2024-05-14 13:54:57 +0700

    Demo

---
+++ source/repos/QLGaraOto/QLGaraOto-dd63047cfbe059d679df5e8211ee688d25a5722f/QLGaraOto/DangKy.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QLGaraOto
+{
+    /// <summary>
+    /// Interaction logic for DangKy.xaml
+    /// </summary>
+    public partial class DangKy : Window
+    {
+        public DangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClosing_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Bạn có muốn đóng chương trình không?", "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (r == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            DangNhap dangNhap = new DangNhap();
+            dangNhap.Show();
+            this.Close();
+        }
+    }
+}
---
+++ source/repos/QLGaraOto/QLGaraOto-dd63047cfbe059d679df5e8211ee688d25a5722f/QLGaraOto/View/HoaDonThanhToanPhuTungUC.xaml.cs
@@ -0,0 +1,31 @@
+﻿using QLGaraOto.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLGaraOto.View
+{
+    /// <summary>
+    /// Interaction logic for HoaDonThanhToanPhuTungUC.xaml
+    /// </summary>
+    public partial class HoaDonThanhToanPhuTungUC : UserControl
+    {
+        public HoaDonThanhToanPhuTung ViewModel { get; set; }
+        public HoaDonThanhToanPhuTungUC()
+        {
+            InitializeComponent();
+            this.DataContext = ViewModel=new HoaDonThanhToanPhuTung();
+        }
+    }
+}
