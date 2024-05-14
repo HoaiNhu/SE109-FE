c1484082fe2d8b4b4d960e7ad9b2084946a7b2d8
tamnhu11204 <22521053@gm.uit.edu.vn>
2024-05-14 21:14:23 +0700

    Demo1

---
+++ OneDrive/Máy tính/QLGaraOto-dd63047cfbe059d679df5e8211ee688d25a5722f/QLGaraOto-dd63047cfbe059d679df5e8211ee688d25a5722f/QLGaraOto/View/NhanVienUC.xaml.cs
@@ -0,0 +1,31 @@
+﻿using QLGaraOto.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLGaraOto.View
+{
+    /// <summary>
+    /// Interaction logic for NhanVienUC.xaml
+    /// </summary>
+    public partial class NhanVienUC : UserControl
+    {
+        public NhanVien ViewModel { get; set; }
+        public NhanVienUC()
+        {
+            InitializeComponent();
+            this.DataContext = ViewModel= new NhanVien();
+        }
+    }
+}
